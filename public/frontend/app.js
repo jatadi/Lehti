b2be695bf542475b7b9f2abfb1ae1bdc4690dd33
Tadija Ciric <tadijaciric@gmail.com>
2025-08-22 01:21:15 +0200

    Changed IP address for non local deployment

--- public/frontend/app.js
+++ public/frontend/app.js
@@ -1,7 +1,7 @@
 // Lehti Health Tracker - Frontend Application
 class LehtiApp {
     constructor() {
-        this.baseURL = 'http://localhost:8000/api';
+        this.baseURL = 'http://192.168.100.172:8000/api';
         this.token = localStorage.getItem('auth_token');
         this.user = JSON.parse(localStorage.getItem('user') || 'null');
         this.currentPage = 'dashboard';
